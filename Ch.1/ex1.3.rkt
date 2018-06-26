3c3f2bdfa8f8d021194f1f52dad0cbe3fee4493f
BinaryArchaism <seriouskeyn@mail.ru>
2018-06-26 22:58:31 +0400

    day1

---
+++ Ch.1/ex1.3.rkt
@@ -0,0 +1,7 @@
+#lang racket
+(define (square x) (* x x))
+(define (f x y z)
+  (cond ((and (<= x y) (<= x z)) (+ (square y) (square z)))
+        ((and (<= y x) (<= y z)) (+ (square x) (square z)))
+        (else (+ (square x) (square y)))))
+  
