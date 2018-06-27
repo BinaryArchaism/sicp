302bfdce9c40d627785d245d8649ca7b23012e54
BinaryArchaism <seriouskeyn@mail.ru>
2018-06-27 13:29:29 +0200

    ex1.7

---
+++ Ch.1/ex1.7.rkt
@@ -0,0 +1,26 @@
+#lang racket
+(define (sqrt x)
+	(sqrt-iter 1.0 x))
+
+(define (sqrt-iter guess x)
+	(if (good-enough? guess x)
+		guess
+		(sqrt-iter (improve guess x)
+			x)))
+
+(define (improve guess x)
+	(average guess(/ x guess)))
+
+(define (average x y)
+	(/ (+ x y) 2))
+
+(define (good-enough? guess x)
+	(< (abs (- (square guess) x)) 0.001)) 
+
+(define (square x)
+	(* x x))
+
+(define (abs x)
+	(if (< x 0) (- x) x))
+  
+;(define (good-enough2? guess prev-guess))
