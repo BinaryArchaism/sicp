3470fd02694f8550a8a65a31e447c7c7d60669c7
BinaryArchaism <seriouskeyn@mail.ru>
2018-06-28 14:35:39 +0200

    28.06

---
+++ Ch.1/ex1.8.rkt
@@ -0,0 +1,15 @@
+#lang racket
+(define (qbrt x) 
+  (define (qube x) (* x x x)) 
+  (define (square x) (* x x)) 
+  (define (qbrt-iter guess) 
+    (define (good-enough?) 
+      (< (abs (- (qube guess) x)) 0.0001)) 
+    (define (improve) 
+      (/ (+ (/ x (square guess)) 
+            (* 2 guess)) 
+         3)) 
+    (if (good-enough?) 
+        guess 
+        (qbrt-iter (improve)))) 
+  (qbrt-iter 1.0))
